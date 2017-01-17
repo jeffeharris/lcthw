f527988df68fdd9cd29bdd702501273297031b41
Jeff Harris <jeffre4828@gmail.com>
2017-01-17 00:15:26 -0500

    complete ex16 extra credit

--- ex16.c
+++ ex16.c
@@ -12,76 +12,48 @@ struct Person {
 	int weight;
 };
 
-// define a pointer to a Person Struct called Person_create that
-// that takes several variables. 
-struct Person *Person_create(char *name, int age, int height,
-	int weight)
-{
-	// define a pointer to a Person struct named who and
-	// allocate the required amount of memory for it
-	struct Person *who = malloc(sizeof(struct Person));
-	// ensure NULL was not returned (a NULL address)
-	assert(who != NULL);
-
-	// assign values to the Person attributes
-	who->name = strdup(name);
-	who->age = age;
-	who->height = height;
-	who->weight = weight;
-
-	// return the created Person named who
-	return who;
-}
-
-// define a function that clears the memory allocated for a Person struct
-void Person_destroy(struct Person *who)
-{
-	// ensure the Person passed in is not NULL
-	assert(who != NULL);
-
-	// release the memory that had been allocated for the Person
-	free(who->name);
-	free(who);
-}
-
 // define a function that prints to the screen the Person attributes
-void Person_print(struct Person *who)
+void Person_print(struct Person who)
 {
 	// print the Persons attributes
-	printf("Name: %s\n", who->name);
-	printf("\tAge: %d\n", who->age);
-	printf("\tHeight: %d\n", who->height);
-	printf("\tWeight: %d\n", who->weight);
+	printf("Name: %s\n", who.name);
+	printf("\tAge: %d\n", who.age);
+	printf("\tHeight: %d\n", who.height);
+	printf("\tWeight: %d\n", who.weight);
 }
 
 // define Main function
 int main(int argc, char *argv[])
 {
 	// make two people structures
-	struct Person *joe = Person_create("Joe Alex", 32, 64, 140);
+	struct Person joe;
+	joe.name = "Joe Alex";
+	joe.age = 32;
+	joe.height = 64;
+	joe.weight = 140;
 
-	struct Person *frank = Person_create("Frank Blank", 20, 72, 180);
+	struct Person frank;
+	frank.name = "Frank Blank";
+	frank.age = 20;
+	frank.height = 72;
+	frank.weight = 180;
 
 	// print them out and where they are in memory
-	printf("Joe is at memory location %p:\n", joe);
+	printf("Joe is at memory location %p:\n", &joe);
 	Person_print(joe);
 
-	printf("Frank is at memory location %p:\n", frank);
+	printf("Frank is at memory location %p:\n", &frank);
 	Person_print(frank);
 
 	// make everyone age 20 years and print them again
-	joe->age += 20;
-	joe->height -= 2;
-	joe->weight += 40;
+	joe.age += 20;
+	joe.height -= 2;
+	joe.weight += 40;
 	Person_print(joe);
 
-	frank->age += 20;
-	frank->weight += 20;
+	frank.age += 20;
+	frank.weight += 20;
 	Person_print(frank);
-
-	// destroy them both so we clean up
-	Person_destroy(joe);
-	Person_destroy(frank);
 
 	return 0;
 }
